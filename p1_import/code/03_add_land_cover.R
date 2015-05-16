98178c648e202deb14087a7cd59dce485d611452
Alison Appling <aappling@usgs.gov>
2015-05-16 10:17:31 -0500

    try geoknife for powstreams

--- p1_import/code/03_add_land_cover.R
+++ p1_import/code/03_add_land_cover.R
@@ -12,6 +12,7 @@ library(dataRetrieval)
 library(sbtools)
 library(mda.streams)
 library(powstreams)
+library(magrittr)
 
 # Enter sandbox
 # sbtools::set_endpoint("production") # the default
@@ -31,4 +32,66 @@ site_roots <- item_list_children(sites_root$id, current_session(), limit=1000)$i
 sites <- sapply(site_roots, mda.streams:::get_title, session=current_session()) #get_title should probably be exported
 # alternately: sites <- query_item_identifier(scheme = "mda_streams_dev", type = "site_root", session = current_session(), limit = 1000)
 
-# geoknife script goes here
+# Add geoknife
+for (n in 1:length(sites)){
+  site <- sites[n]
+  
+  # Get the data. This section breaks, probably because watershed_wfs is a private url. How to get around that?
+  watershed_wfs <- 
+    query_item_identifier(scheme = 'mda_streams_dev', type = 'watershed', key = site) %$% # cool magrittr pipe: exposes names to next fun
+    item_get(id) %>%
+    mda.streams:::match_url_distro("ScienceBase WFS Service")
+  stencil <- webgeom(url=watershed_wfs)
+  fabric <- webdata(url="http://cida.usgs.gov/thredds/dodsC/mows/sr")
+  job <- geoknife(stencil, fabric)
+  check(job) #ultimately get ProcessFailed
+  watershed_data <- loadOutput(job)
+  
+  # The example is nice and works fine
+  #   stencil <- webgeom("state::CO")
+  #   fabric <- webdata(list(
+  #     times = as.POSIXct(c('1895-01-01','1899-01-01')),
+  #     url = 'http://cida.usgs.gov/thredds/dodsC/prism',
+  #     variables = 'ppt'))
+  #   job <- geoknife(stencil, fabric) #ignore the checkAttrNamespaces warning for now - it's a known issue
+  #   check(job)$status # get Process successful
+  #   data <- loadOutput(job)
+  #   plot(data[,1:2], ylab = variables(fabric))
+
+  # This example doesn't work
+  #   stencil <- webgeom("state::CO")
+  #   fabric <- webdata(url="http://cida.usgs.gov/thredds/dodsC/mows/sr")
+  #   job <- geoknife(stencil, fabric) #ignore the checkAttrNamespaces warning for now - it's a known issue
+  #   check(job) # breaks. won't show or terminate job.
+  
+  # Check for existing item on SB
+  if(!is.null(shed)){
+    exists <- item_exists(scheme = 'mda_streams_dev',type = 'geostat', key = site)
+  } else {
+    exists = NULL
+  }
+  
+  # Write data to file, then post to SB. Switch to more descriptive name that
+  # geostat once we know what can be acquired
+  if (watershed_data_getting_worked && !exists){
+
+    # write to file here
+    
+    # as in post_watershed(site = site, files = shp_files, session = session)
+    #     check = query_item_identifier('mda_streams_dev','geostat', site, current_session())
+    #     if(nrow(check) == 0) {
+    #       site_root <- query_item_identifier(scheme='mda_streams_dev', type='site_root',key=site, current_session())
+    #       ws_id <- item_upload_create(site_root$id, files=shp_files, session=current_session())
+    #       item_update_identifier(ws_id, scheme='mda_streams_dev', type='geostat', key=site, session=current_session())
+    #       item_update(ws_id, list('title'=jsonlite::unbox('geostat')), current_session())
+    #       cat('posting ');cat(shp_files); cat('\n')
+    #     } else {
+    #       cat('not posting ');cat(shp_files); cat('; already exists\n')
+    #     }
+    
+    # clean up locally written files
+    #    unlink(shp_files)
+  } else {
+    print(paste0('skipping ',site))
+  }
+}
