2082e8cdd91a4762cbad9e91e117ff1f0c847c93
Alison Appling <aappling@usgs.gov>
2015-05-15 21:04:09 -0500

    two working import scripts, sans make

--- p1_import/code/00_init_sites.R
+++ p1_import/code/00_init_sites.R
@@ -8,60 +8,40 @@ cat(paste0("Setting up site data as ", sb_user, "\n"))
 
 
 # Load libraries
-
 library(dataRetrieval)
-library(mda.streams)
 library(sbtools)
+library(mda.streams)
+library(powstreams)
 
+# Enter sandbox
+# sbtools::set_endpoint("production") # the default
+# sbtools::set_endpoint("development") # this doesn't work off campus (except maybe with VPN?)
 
 # Log in
-if(!missing(sb_password)) 
+if(exists("sb_password")) 
   session = authenticate_sb(sb_user, sb_password) 
 else 
   session = authenticate_sb(sb_user)
 
+# find the project root ("Continental Stream Metabolism" folder)
+true_sites_root <- sbtools::query_item_identifier(scheme="mda_streams", type="project_root", key="uber")$id
+project_root <- sbtools::item_get_parent(true_sites_root)
 
-#' Identify list of sites to import.
-#' 
-#' For a site to qualify, it must be in ANY of the passed-in stateCd values and
-#' have data for ALL of the passed-in p_codes values
-identify_nwis_sites <- function(p_codes=c('00010', '00060', '00095', '00300'), stateCd="all") {
-  pcodes <- dataRetrieval::readNWISpCode(p_codes)
-  cat("Requiring all of the following parameter codes:\n")
-  print(pcodes)
-  gsub("nwis-", "nwis_", mda.streams::init_nwis_sites(p_codes, stateCd))
+# create a sandbox sites folder to work with from here on
+# only do once (need to find way to try these lines without damaging things if the )
+sites_root <- sbtools::query_item_identifier(scheme="mda_streams_dev", type="sites_root", key="uber", session=current_session())
+if(nrow(sites_root) == 0) {
+  sites_root <- sbtools::item_create(parent_id = project_root, title="Sites_dev")
+  sbtools::item_update_identifier(id=sites_root, scheme="mda_streams_dev", type="sites_root", key="uber") # true sites root currently has type="project_root". i find that confusing.
 }
-site_ids <- identify_nwis_sites(p_codes=c('00010', '00060', '00095', '00300'), stateCd=c("wi"))
 
-#' Create site items on SB
-#' 
-#' Much of the functionality here, which I largely copied from the demo file in
-#' mda.streams, is actually in newer functions now. So this function is
-#' partially obsolete.
-create_SB_sites <- function(site_ids, parent_sb_item='5487139fe4b02acb4f0c8110', session) {
-
-  # Add any items that don't already exist
-  for(i in 1:length(site_ids)){
-    
-    # If the item already exists, skip it
-    tmp = sbtools::query_item_identifier('mda_streams','site_root', site_ids[i], session)
-    exists <- item_exists("mda_streams", "site_root", site_ids[1], session)
-    if(nrow(tmp) > 0) {
-      warning('skipping site ', site_ids[i], ', already exists. SBid:', tmp[1,]$id)
-      next
-    }
-    
-    # Otherwise, create the item as a child of parent_sb_item
-    id = item_create(parent_id = parent_sb_item, title = site_ids[i], session = session)
-    item_update_identifier(id, 'mda_streams', 'site_root', site_ids[i], session)
+# get list of sites from NWIS & put list on SB
+sites_list <- mda.streams::init_nwis_sites(p_codes=c('00010', '00060', '00300'), stateCd="CO")
+create_sites_out <- lapply(sites_list, function(site) {
+  # like mda.streams::create_site(site)
+  site_root <- query_item_identifier(scheme="mda_streams_dev", type="site_root", key=site, session=current_session())
+  if(nrow(site_root) == 0) {
+    site_root_id <- item_create(parent_id=sites_root$id, title=site)
+    item_update_identifier(site_root_id, scheme="mda_streams_dev", type="site_root", key=site)
   }
-  
-  # Check for any items under this parent that aren't in site_ids
-  all_items <- item_list_children(parent_sb_item, session=session, limit=10000)
-  item_get(id=all_items$id[1])
-}
-create_SB_sites(site_ids=site_ids, parent_sb_item='5487139fe4b02acb4f0c8110', session)
-
-#' Add time series data to SB
-
-#' Add watershed delineations to SB
+})
