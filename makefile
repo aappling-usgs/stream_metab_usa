799a5d8bd072205f0c6f346b10a71c195cf0047c
Alison Appling <aappling@usgs.gov>
2015-06-03 11:51:24 -0500

    init_sites & reinit_sites working and run

--- makefile
+++ makefile
@@ -7,20 +7,28 @@
 
 CALL_R = R CMD BATCH  --no-save --no-restore --slave --no-timing
 
-# Targets
+# Special targets
 
-all : p2_metab
+reinit_sites : p1_import/code/process_make_args.R p1_import/code/00_init_sites.R
+	$(CALL_R) "--args sb_user=\"$(SBUSER)\" sb_password=\"$(SBPASS)\" outfile=$@ update_sitelist=TRUE replace_existing=FALSE delete_all=TRUE verbose=TRUE" p1_import/code/00_init_sites.R p1_import/out/00_reinit_sites.Rout
+
+# Main targets
+
+all : p1_import
+
+#p2_metab
 
 # Rules
 
 ## p1_import
 
-#p1_import : init_sites
+p1_import : init_sites 
+#add_nwis_data add_nldas_data add_calc_data
 
-#init_sites : p1_import/out/00_init_sites.Rout
+init_sites : p1_import/out/00_init_sites.Rout
 
-#p1_import/out/00_init_sites.Rout : p1_import/code/00_init_sites.R
-#	$(CALL_R) "--args sb_user=SBUSER sb_password=SBPASS outfile='$@'" p1_import/code/00_init_sites.R $@
+p1_import/out/00_init_sites.Rout : p1_import/code/00_init_sites.R p1_import/code/process_make_args.R
+	$(CALL_R) "--args sb_user=$(SBUSER) sb_password=$(SBPASS) outfile=$@ update_sitelist=TRUE replace_existing=FALSE delete_all=FALSE verbose=TRUE" p1_import/code/00_init_sites.R $@
 
 #import_tsdata
 #import_watershed
@@ -29,12 +37,12 @@ all : p2_metab
 
 ## p2_metab
 
-p2_metab : model_metab
+#p2_metab : model_metab
 
-model_metab : p2_metab/out/model_metab.Rout
+#model_metab : p2_metab/out/model_metab.Rout
 
-p2_metab/out/model_metab.Rout : 
-	$(CALL_R) "--args sb_user=SBUSER sb_password=SBPASS outfile='$@'" p1_import/code/00_init_sites.R $@
+#p2_metab/out/model_metab.Rout : 
+#$(CALL_R) "--args sb_user=SBUSER sb_password=SBPASS outfile='$@'" p1_import/code/00_init_sites.R $@
 
 ## p3_auxvars
 
--- p1_import/code/00_init_sites.R
+++ p1_import/code/00_init_sites.R
@@ -1,47 +1,34 @@
-# Parse command-line arguments
+source("p1_import/code/process_make_args.R")
+args <- process_make_args(c("sb_user", "sb_password", "outfile", "update_sitelist","replace_existing", "delete_all", "verbose"))
 
-args <- commandArgs(TRUE)
-for (i in 1:length(args)){
-  eval(parse(text=args[i])) # expecting args sb_user, sb_password, and outfile
-}
-cat(paste0("Setting up site data as ", sb_user, "\n"))
-
-
-# Load libraries
-library(dataRetrieval)
-library(sbtools)
-library(mda.streams)
-library(powstreams)
-
-# Enter sandbox
-# sbtools::set_endpoint("production") # the default
-# sbtools::set_endpoint("development") # this doesn't work off campus (except maybe with VPN?)
-
-# Log in
-if(exists("sb_password")) 
-  session = authenticate_sb(sb_user, sb_password) 
-else 
-  session = authenticate_sb(sb_user)
-
-# find the project root ("Continental Stream Metabolism" folder)
-true_sites_root <- sbtools::query_item_identifier(scheme="mda_streams", type="project_root", key="uber")$id
-project_root <- sbtools::item_get_parent(true_sites_root)
-
-# create a sandbox sites folder to work with from here on
-# only do once (need to find way to try these lines without damaging things if the )
-sites_root <- sbtools::query_item_identifier(scheme="mda_streams_dev", type="sites_root", key="uber", session=current_session())
-if(nrow(sites_root) == 0) {
-  sites_root <- sbtools::item_create(parent_id = project_root, title="Sites_dev")
-  sbtools::item_update_identifier(id=sites_root, scheme="mda_streams_dev", type="sites_root", key="uber") # true sites root currently has type="project_root". i find that confusing.
-}
-
-# get list of sites from NWIS & put list on SB
-sites_list <- mda.streams::init_nwis_sites(p_codes=c('00010', '00060', '00300'), stateCd="CO")
-create_sites_out <- lapply(sites_list, function(site) {
-  # like mda.streams::create_site(site)
-  site_root <- query_item_identifier(scheme="mda_streams_dev", type="site_root", key=site, session=current_session())
-  if(nrow(site_root) == 0) {
-    site_root_id <- item_create(parent_id=sites_root$id, title=site)
-    item_update_identifier(site_root_id, scheme="mda_streams_dev", type="site_root", key=site)
+init_sites <- function(update_sitelist=TRUE, replace_existing=FALSE, delete_all=FALSE, verbose=TRUE) {
+  # delete everything before starting (optional)
+  if(delete_all) {
+    if(verbose) message("deleting all sites on ScienceBase")
+    sites_sb <- get_sites()
+    mda.streams:::delete_site(sites_sb, verbose=verbose) 
   }
-})
+  
+  # get sitelist from NWIS (optional)
+  if(update_sitelist) {
+    if(verbose) message("acquiring site list from NWIS server")
+    sites_file <- stage_nwis_sitelist(p_codes=get_var_codes("doobs","p_code"), state_codes=c("all"), folder="p1_import/doc", verbose=verbose)
+  } else {
+    sites_file <- "p1_import/doc/nwis_sites.txt"
+  }
+  
+  # post/repost sites to SB
+  sites_nwis <- readLines(sites_file)
+  if(verbose) message("posting sites with replace_existing==",replace_existing)
+  posted_sites <- post_site(sites_nwis, replace_existing=replace_existing, verbose=verbose)
+  
+  # print results & return
+  if(verbose) {
+    sites_display <- 
+      data.frame(site=names(posted_sites), posted_id=posted_sites, stringsAsFactors=FALSE) %>%
+      transmute(site, existing_id=ifelse(is.na(posted_id), locate_site(site), NA), posted_id)
+    print(sites_display)
+  }
+  return()
+}
+init_sites(update_sitelist=args$update_sitelist, replace_existing=args$replace_existing, delete_all=args$delete_all, verbose=args$verbose)
