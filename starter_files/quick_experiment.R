2f778a1b2c843297740e6ff85a95b135a13dfb78
Alison Appling <aappling@usgs.gov>
2015-07-01 13:43:54 -0500

    experiment changes from week in laramie

--- starter_files/quick_experiment.R
+++ starter_files/quick_experiment.R
@@ -62,6 +62,31 @@ most_data <- most_data[keep_rows[1]:keep_rows[2], ]
 # count the number of observations per day (24-hr period)
 obs_per_day <- nrow(most_data[as.character(as.Date(most_data$date.time)) == "2014-03-10", ])
 
+#### try out metab_bayes_simple ####
+# doobs
+p <- data.frame(do1=7.6, GPP=2, ER=-2.6, k=15, stream="hiGPPER hik", error="none", oesd=0, oephi=0, pesd=0, pephi=0)
+GPP.frac <- most_data$light/sum(most_data$light[as.character(as.Date(most_data$date.time))=="2014-03-10"])
+doobs <- calc_DO_mod_with_error(
+  GPP.daily = p$GPP, ER.daily = p$ER, K600.daily = p$k, 
+  DO.sat = most_data$DO.sat, depth = most_data$depth, temp.water = most_data$temp.water, 
+  frac.GPP = GPP.frac, frac.ER = 1/obs_per_day, frac.D = 1/obs_per_day, 
+  DO.mod.1 = p$do1, n = nrow(most_data), 
+  obs.err.sd = p$oesd, obs.err.phi = p$oephi, proc.err.sd = p$pesd, proc.err.phi = p$pephi)
+plot(doobs)
+bayes_data <- mutate(most_data, DO.obs=doobs)
+#model
+# jags_data <- prepjags_bayes_simple(bayes_data)
+# jags_out <- runjags_bayes_simple(jags_data, maxCores=4, adaptSteps=100, burnInSteps=400, numSavedSteps=1000, thinSteps=10)
+# jags_out
+bayes_out <- metab_bayes_simple(bayes_data, maxCores=4, adaptSteps=1000, burnInSteps=1000, numSavedSteps=1000, thinSteps=1)
+predict_metab(bayes_out)
+bayes_preds <- predict_DO(bayes_out)
+ggplot(bayes_preds, aes(x=date.time, group=date)) + geom_point(aes(y=DO.obs), color='blue') + geom_line(aes(y=DO.mod), color='red') + theme_bw()
+
+#### try out metab_mle ####
+
+metab_mle(data=bayes_data, calc_DO_fun=calc_DO_mod_by_diff)
+
 #### specify and run experiments ####
 
 # params for model experiment runs
@@ -83,11 +108,13 @@ params <-
 params <- params %>%
   mutate(error = ordered(error, levels=params[0+seq(1,nrow(params),by=numstreams),"error"]))
 
-true_ests <- bind_rows(lapply(1:3, function(iter) {
+fits <- NULL
+true_est <- NULL
+true_ests <- bind_rows(lapply(1, function(iter) {
   
   print(paste("iteration", iter))
   
-  fits <- lapply(1:nrow(params), function(ip) {
+  fits <<- lapply(1:nrow(params), function(ip) {
     p <- params[ip,]
     
     # doobs
@@ -100,13 +127,15 @@ true_ests <- bind_rows(lapply(1:3, function(iter) {
       obs.err.sd = p$oesd, obs.err.phi = p$oephi, proc.err.sd = p$pesd, proc.err.phi = p$pephi)
     all_data <- mutate(most_data, DO.obs=doobs)
     
-    metab_mle(data=all_data, calc_DO_fun=calc_DO_mod_by_step)
+    #metab_mle(data=all_data, calc_DO_fun=calc_DO_mod_by_diff)
+    metab_bayes_simple(all_data, maxCores=4, adaptSteps=100, burnInSteps=500, numSavedSteps=500, thinSteps=1)
   })
   
-  true_est <- bind_rows(lapply(1:length(fits), function(fitnum) {
+  true_est <<- bind_rows(lapply(1:length(fits), function(fitnum) {
     p <- params[fitnum,c("GPP","ER","k","stream","error","oesd","pesd","pephi")]
     fit <- fits[[fitnum]]
-    est <- setNames(predict_metab(fit)[2,c("GPP","ER","K600")], c("GPP","ER","k"))
+    #est <- mutate(predict_metab(fit)[2,c("GPP","ER","K600")], GPP.sd=NA, ER.sd=NA, K600.sd=NA)
+    est <- get_fit(fit)[2,c("GPP","GPP.sd","ER","ER.sd","K600","K600.sd")]
     data.frame(true=p, est=est)
   })) %>% mutate(iter=iter)
   
@@ -114,35 +143,39 @@ true_ests <- bind_rows(lapply(1:3, function(iter) {
 }))
 #save(params, true_ests, file="starter_files/quick_experiment_2.RData")
 
+# true_est_bayes <- true_est
+# true_ests_bayes <- true_ests
+# fits_bayes <- fits
+
 #### plot model metrics/errors ####
 
 # one of each error type
 oneeach <- filter(true_ests, true.error%in%c("none","obs2","proc2","autoproc2"))
-ggplot(oneeach, aes(x=true.k, y=est.k, color=true.error, shape=true.stream)) + geom_abline(color="grey") + geom_point(size=2, alpha=0.9, position=position_jitter(width=.2, height=0)) + theme_bw() + scale_shape_discrete(solid=FALSE)
+ggplot(oneeach, aes(x=true.k, y=est.K600, color=true.error, shape=true.stream)) + geom_abline(color="grey") + geom_point(size=2, alpha=0.9, position=position_jitter(width=.2, height=0)) + theme_bw() + scale_shape_discrete(solid=FALSE)
 ggplot(oneeach, aes(x=true.ER, y=est.ER, color=true.error, shape=true.stream)) + geom_abline(color="grey") + geom_point(size=2, alpha=0.9, position=position_jitter(width=.2, height=0)) + theme_bw() + scale_shape_discrete(solid=FALSE)
 ggplot(oneeach, aes(x=true.GPP, y=est.GPP, color=true.error, shape=true.stream)) + geom_abline(color="grey") + geom_point(size=2, alpha=0.9, position=position_jitter(width=.2, height=0)) + theme_bw() + scale_shape_discrete(solid=FALSE)
 
 # variation in obs error
 obserrs <- filter(true_ests, true.pesd==0)
-ggplot(obserrs, aes(x=true.oesd, y=est.k-true.k, color=true.stream)) + geom_point(size=2, alpha=0.9, position=position_jitter(width=.005, height=0)) + theme_bw() + scale_shape_discrete(solid=FALSE) + facet_wrap(~true.stream, scales="free_y") + scale_color_discrete(guide=FALSE)
-ggplot(obserrs, aes(x=true.oesd, y=est.ER-true.ER, color=true.stream)) + geom_point(size=2, alpha=0.9, position=position_jitter(width=.005, height=0)) + theme_bw() + scale_shape_discrete(solid=FALSE) + facet_wrap(~true.stream, scales="free_y") + scale_color_discrete(guide=FALSE)
-ggplot(obserrs, aes(x=true.oesd, y=est.GPP-true.GPP, color=true.stream)) + geom_point(size=2, alpha=0.9, position=position_jitter(width=.005, height=0)) + theme_bw() + scale_shape_discrete(solid=FALSE) + facet_wrap(~true.stream, scales="free_y") + scale_color_discrete(guide=FALSE)
+ggplot(obserrs, aes(x=true.oesd, y=est.K600-true.k, color=true.stream)) + geom_point(size=2, alpha=0.9, position=position_jitter(width=.005, height=0)) + theme_bw() + scale_shape_discrete(solid=FALSE) + facet_wrap(~true.stream, scales="free_y") + scale_color_discrete(guide=FALSE) + geom_errorbar(aes(y=0, ymin=-1.96*est.K600.sd, ymax=1.96*est.K600.sd))
+ggplot(obserrs, aes(x=true.oesd, y=est.ER-true.ER, color=true.stream)) + geom_point(size=2, alpha=0.9, position=position_jitter(width=.005, height=0)) + theme_bw() + scale_shape_discrete(solid=FALSE) + facet_wrap(~true.stream, scales="free_y") + scale_color_discrete(guide=FALSE) + geom_errorbar(aes(y=0, ymin=-1.96*est.ER.sd, ymax=1.96*est.ER.sd))
+ggplot(obserrs, aes(x=true.oesd, y=est.GPP-true.GPP, color=true.stream)) + geom_point(size=2, alpha=0.9, position=position_jitter(width=.005, height=0)) + theme_bw() + scale_shape_discrete(solid=FALSE) + facet_wrap(~true.stream, scales="free_y") + scale_color_discrete(guide=FALSE) + geom_errorbar(aes(y=0, ymin=-1.96*est.GPP.sd, ymax=1.96*est.GPP.sd))
 
 # variation in proc error
 procerrs <- filter(true_ests, true.oesd==0 & true.pephi==0)
-ggplot(procerrs, aes(x=true.pesd, y=est.k-true.k, color=true.stream)) + geom_point(size=2, alpha=0.9, position=position_jitter(width=.0005, height=0)) + theme_bw() + scale_shape_discrete(solid=FALSE) + facet_wrap(~true.stream, scales="free_y") + scale_color_discrete(guide=FALSE)
-ggplot(procerrs, aes(x=true.pesd, y=est.ER-true.ER, color=true.stream)) + geom_point(size=2, alpha=0.9, position=position_jitter(width=.0005, height=0)) + theme_bw() + scale_shape_discrete(solid=FALSE) + facet_wrap(~true.stream, scales="free_y") + scale_color_discrete(guide=FALSE)
-ggplot(procerrs, aes(x=true.pesd, y=est.GPP-true.GPP, color=true.stream)) + geom_point(size=2, alpha=0.9, position=position_jitter(width=.0005, height=0)) + theme_bw() + scale_shape_discrete(solid=FALSE) + facet_wrap(~true.stream, scales="free_y") + scale_color_discrete(guide=FALSE)
+ggplot(procerrs, aes(x=true.pesd, y=est.K600-true.k, color=true.stream)) + geom_point(size=2, alpha=0.9, position=position_jitter(width=.0005, height=0)) + theme_bw() + scale_shape_discrete(solid=FALSE) + facet_wrap(~true.stream, scales="free_y") + scale_color_discrete(guide=FALSE) + geom_errorbar(aes(y=0, ymin=-1.96*est.K600.sd, ymax=1.96*est.K600.sd))
+ggplot(procerrs, aes(x=true.pesd, y=est.ER-true.ER, color=true.stream)) + geom_point(size=2, alpha=0.9, position=position_jitter(width=.0005, height=0)) + theme_bw() + scale_shape_discrete(solid=FALSE) + facet_wrap(~true.stream, scales="free_y") + scale_color_discrete(guide=FALSE) + geom_errorbar(aes(y=0, ymin=-1.96*est.ER.sd, ymax=1.96*est.ER.sd))
+ggplot(procerrs, aes(x=true.pesd, y=est.GPP-true.GPP, color=true.stream)) + geom_point(size=2, alpha=0.9, position=position_jitter(width=.0005, height=0)) + theme_bw() + scale_shape_discrete(solid=FALSE) + facet_wrap(~true.stream, scales="free_y") + scale_color_discrete(guide=FALSE) + geom_errorbar(aes(y=0, ymin=-1.96*est.GPP.sd, ymax=1.96*est.GPP.sd))
 
 # variation in proc error
 aperrs <- filter(true_ests, substr(true.error, 1, 4) %in% c("none","auto"))
-ggplot(aperrs, aes(x=true.pephi, y=est.k-true.k, color=true.stream)) + geom_point(size=2, alpha=0.9, position=position_jitter(width=.0005, height=0)) + theme_bw() + scale_shape_discrete(solid=FALSE) + facet_wrap(~true.stream, scales="free_y") + scale_color_discrete(guide=FALSE)
-ggplot(aperrs, aes(x=true.pephi, y=est.ER-true.ER, color=true.stream)) + geom_point(size=2, alpha=0.9, position=position_jitter(width=.0005, height=0)) + theme_bw() + scale_shape_discrete(solid=FALSE) + facet_wrap(~true.stream, scales="free_y") + scale_color_discrete(guide=FALSE)
-ggplot(aperrs, aes(x=true.pephi, y=est.GPP-true.GPP, color=true.stream)) + geom_point(size=2, alpha=0.9, position=position_jitter(width=.0005, height=0)) + theme_bw() + scale_shape_discrete(solid=FALSE) + facet_wrap(~true.stream, scales="free_y") + scale_color_discrete(guide=FALSE)
+ggplot(aperrs, aes(x=true.pephi, y=est.K600-true.k, color=true.stream)) + geom_point(size=2, alpha=0.9, position=position_jitter(width=.0005, height=0)) + theme_bw() + scale_shape_discrete(solid=FALSE) + facet_wrap(~true.stream, scales="free_y") + scale_color_discrete(guide=FALSE) + geom_errorbar(aes(y=0, ymin=-1.96*est.K600.sd, ymax=1.96*est.K600.sd))
+ggplot(aperrs, aes(x=true.pephi, y=est.ER-true.ER, color=true.stream)) + geom_point(size=2, alpha=0.9, position=position_jitter(width=.0005, height=0)) + theme_bw() + scale_shape_discrete(solid=FALSE) + facet_wrap(~true.stream, scales="free_y") + scale_color_discrete(guide=FALSE) + geom_errorbar(aes(y=0, ymin=-1.96*est.ER.sd, ymax=1.96*est.ER.sd))
+ggplot(aperrs, aes(x=true.pephi, y=est.GPP-true.GPP, color=true.stream)) + geom_point(size=2, alpha=0.9, position=position_jitter(width=.0005, height=0)) + theme_bw() + scale_shape_discrete(solid=FALSE) + facet_wrap(~true.stream, scales="free_y") + scale_color_discrete(guide=FALSE) + geom_errorbar(aes(y=0, ymin=-1.96*est.GPP.sd, ymax=1.96*est.GPP.sd))
 aperrs %>% group_by(true.stream, true.error) %>% summarize(
   GPP.err=mean(est.GPP-true.GPP), GPP.err.sd=sd(est.GPP-true.GPP),
   ER.err=mean(est.ER-true.ER), ER.err.sd=sd(est.ER-true.ER), 
-  k.err=mean(est.k-true.k), k.err.sd=sd(est.k-true.k))
+  k.err=mean(est.K600-true.k), k.err.sd=sd(est.K600-true.k))
 
 #### plot the DO observations and predictions ####
 
