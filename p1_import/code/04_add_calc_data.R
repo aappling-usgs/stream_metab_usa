7e1068337232ca904fa1e4c03f88bc5fdee9a028
Alison Appling <aappling@usgs.gov>
2015-07-06 10:25:30 -0500

    calc suntime, par, depth now up on SB

--- p1_import/code/04_add_calc_data.R
+++ p1_import/code/04_add_calc_data.R
@@ -1,42 +1,54 @@
 source("p1_import/code/process_make_args.R")
-args <- process_make_args(c("sb_user", "sb_password", "var", "on_exists", "verbose"))
+args <- process_make_args(c("sb_user", "sb_password", "var_src", "on_exists", "verbose"))
 
-#' Pull data from NWIS onto ScienceBase
+#' Pull data from calc onto ScienceBase
 #' 
-#' Include data for all variables with src="NWIS" in var_codes, all sites
+#' Include data for all variables with src="calc" in var_codes, all sites
 #' currently on SB, and the date range specified
-add_styx_data <- function(var="doobs", on_exists="skip", sb_user, sb_password, verbose=TRUE) {
+add_calc_data <- function(var_src="suntime_calcLon", on_exists="stop", sb_user, sb_password, verbose=TRUE) {
+  # identify the data to download
   
-  # create the sites
-  sites_styx001 <- sprintf("styx_001%03d", 13)
-  system.time(posted_sites <- post_site(sites_styx001, on_exists=on_exists, verbose=verbose))
-  
-  # find a good real-data site to start from
-  #   dat <- read_ts(download_ts("wtr_nwis", sites[140], on_local_exists="skip"))
-  #   sumry <- dat %>% v() %>% mutate(Date=as.Date(DateTime)) %>% group_by(Date) %>% summarize(wtr_min=min(wtr), wtr_max=max(wtr), wtr_range=max(wtr)-min(wtr))
-  #   which(sumry$wtr_min > 9 & sumry$wtr_min < 11 & sumry$wtr_range > 4 & sumry$wtr_range < 7)
-  #   ggplot(gather(sumry, type, val, 2:4), aes(x=Date, y=val, color=type)) + geom_line()
-  
-  # sites(140) = nwis_02207135, dataRetrieval::readNWISsite("02207135"), 
-  # sumry[which(sumry$wtr_min > 9 & sumry$wtr_min < 11 & sumry$wtr_range > 4 & 
-  # sumry$wtr_range < 7),] gives 12, including 3 in a row on 2014-03-9 to
-  # 2014-03-11
-  
-  # start with real water temperature and coord data
-  var <- "wtr_nwis"
-  real_site <- "nwis_02207135"
-  times <- data.frame(start_date=as.POSIXct("2014-03-08 22:30:00", tz="UTC"), 
-                      end_date=as.POSIXct("2014-03-12 06:00:00", tz="UTC"), stringsAsFactors = FALSE)
+  if(length(var_src) != 1) stop("expecting exactly 1 var_src")
+  var <- parse_var_src(var_src)$var
+  src <- parse_var_src(var_src)$src
+  sites <- sort(get_sites())
   if(verbose) {
-    message("will get data for these parameter codes: ", paste0(vars, collapse=", "))
-    message("will get data between these dates: ", paste0(times, collapse=", "))
-    message("will get data for site ", site)
+    message("will get data for this var_src: ", var_src)
+    message("will calculate data for up to ", length(sites), " sites")
+    message("will download temp data to ", tempdir())
+  }
+
+  # determine list of sites we ACTUALLY need to compute
+  sites_to_get <- sites
+  if(on_exists %in% c("stop", "skip")) {
+    if(verbose) message("checking for existing data...")
+    ts_locs <- locate_ts(var_src=var_src, site_name=sites)
+    if(on_exists == "stop" && length(ts_already <- which(!is.na(ts_locs))) > 0)
+      stop("on_exists='stop' and ", length(ts_already), " sites already have data: ", paste0(sites[ts_already], collapse=","))
+    sites_to_get <- sites_to_get[is.na(ts_locs)]
+  }
+    
+  # loop through groups and var to download and post files.
+  for(site in sites_to_get) {
+
+    # reauthenticate if needed
+    if(is.null(current_session())) {
+      message("re-authenticating with ScienceBase with the password you set.\n")
+      authenticate_sb(sb_user, sb_password) 
+    }
+    
+    # download inputs, calculate data, and post.
+    tryCatch({
+      ts_file <- stage_calc_ts(sites=site, var=var, src=src, verbose=TRUE)
+      post_ts(ts_file, on_exists=on_exists, verbose=TRUE)
+    }, error=function(e) {
+      message("stage or post operation failed for ", site, "; skipping ts")
+      message(e)
+    })
   }
   
-  
-  # clean up
-  message("styx data are fully posted to SB. model away!")
-  writeLines(as.character(Sys.time()), sprintf("p1_import/out/is_ready_styx_%s.txt", var))
+  message("calc data are fully posted to SB for var_src=", var_src)
+  writeLines(as.character(Sys.time()), sprintf("p1_import/out/is_ready_calc_%s.txt", var_src))
   invisible()
 }
-add_styx_data(var=args$var, on_exists=args$on_exists, sb_user=args$sb_user, sb_password=args$sb_password, verbose=args$verbose)
+add_calc_data(var_src=args$var_src, on_exists=args$on_exists, sb_user=args$sb_user, sb_password=args$sb_password, verbose=args$verbose)
