944960d38679ae500c59061dd0210e32c4510de9
Alison Appling <aappling@usgs.gov>
2015-07-24 14:53:55 -0500

    update after launching 0.0.4. fixes #26

--- condor_metab/simple.sh
+++ condor_metab/simple.sh
@@ -3,4 +3,4 @@ export PATH=/usr/local/bin:/bin:/usr/bin:/usr/local/sbin:/usr/sbin:/sbin:/usr/lo
 
 mkdir rLibs
 
-Rscript -e "parallel:::.slaveRSOCK()" MASTER=130.11.177.17 PORT=4043 OUT=log.log TIMEOUT=2592000 METHODS=TRUE XDR=TRUE 
+Rscript -e "parallel:::.slaveRSOCK()" MASTER=130.11.177.17 PORT=$2 OUT=$1.log TIMEOUT=2592000 METHODS=TRUE XDR=TRUE 
