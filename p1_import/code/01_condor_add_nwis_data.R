8606b97da3e52040f9e2052ea3982729e1285682
Alison Appling <aappling@usgs.gov>
2015-07-30 14:57:21 -0500

    more script tweaks as using condor to run models

--- p1_import/code/01_condor_add_nwis_data.R
+++ p1_import/code/01_condor_add_nwis_data.R
@@ -3,6 +3,12 @@ clusterCall(c1, function() { library(mda.streams) })
 clusterCall(c1, function() { library(sbtools) })
 
 ## manually define SBUSER and SBPASS in console
+args <- list(sb_user=SBUSER, sb_password=SBPASS)
+clusterExport(c1, 'args')
+clusterCall(c1, function() { sbtools::authenticate_sb(args$sb_user, args$sb_password) })
+needs_auth <- which(unlist(clusterCall(c1, function() { is.null(sbtools::current_session()) })))
+clusterCall(c1[needs_auth], function() { sbtools::authenticate_sb(args$sb_user, args$sb_password) })
+needs_auth <- which(unlist(clusterCall(c1, function() { is.null(sbtools::current_session()) })))
 
 # choose sites - currently specific to depth_calcDischHarvey
 library(mda.streams)
@@ -11,13 +17,10 @@ dvqcoefs <- dvqcoefs[complete.cases(dvqcoefs[c('dvqcoefs.k','dvqcoefs.m')]),]
 sites <- dvqcoefs$site_name #list_sites()
 sites=intersect(sites, list_sites('disch_nwis'))
 
-# here for depth_calcDischRaymond
-sites <- list_sites('disch_nwis')
+# here for veloc_calcDischRaymond
+sites <- list_sites(c('veloc_calcDischRaymond','veloc_calcDischHarvey'), logic='any')
 
-# here for par_calcSw
-sites <- list_sites('sw_nldas')
-
-args <- list(var_src='par_calcSw', sites=sites, on_exists='skip', sb_user=SBUSER, sb_password=SBPASS, verbose=TRUE)
+args <- list(var_src='velocdaily_calcDMean', sites=sites, on_exists='skip', sb_user=SBUSER, sb_password=SBPASS, verbose=TRUE)
 clusterExport(c1, 'args')
 ## manually source the add_calc_data function (but not the call to it)
 clusterExport(c1, 'add_calc_data')
@@ -35,13 +38,15 @@ clusterExport(c1, 'args')
 site_nums <- 1:length(args$sites)
 all_out <- clusterApplyLB(c1, site_nums, condor_add_calc_data)
 
-# and if you need to edit mda.streams:
+# and if you need to edit mda.streams (but be careful - there's something strange about sbauth)
 clusterCall(c1, function() { 
   detach(package:mda.streams,unload=TRUE)
   install_check('github','mda.streams','aappling-usgs')
   library(mda.streams)
 })
 
-clusterCall(c1, function() { 
-  sbtools::authenticate_sb(args$sb_user, args$sb_password)
-})
+# to delete a ts:
+length(sites <- list_sites('K600_estDOMLEPRK'))
+clusterExport(c1, 'sites')
+site_nums <- 1:length(sites)
+clusterApplyLB(c1, site_nums, function(i) { mda.streams:::delete_ts('gpp_estDOMLEPRK', sites[i]) })
