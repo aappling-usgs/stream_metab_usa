4dcc52d5bd92122f4e17c2139f11aa8bbed92f44
Jordan S Read <jread@usgs.gov>
2016-06-21 12:43:03 -0500

    adding GLDAS to make

--- p1_import/code/00_subset_nldas.R
+++ p1_import/code/00_subset_nldas.R
@@ -103,10 +103,8 @@ nccopy_ldas <- function(file.list, mssg.file, internal.config){
     v <- strsplit(x,'[.]')[[1]]
     sprintf("[%s:1:%s]",v[1],v[2])
   }
-  # 
-  # registerDoMC(cores=4)
-  # foreach(file=files$file) %dopar% {
-  for (file in files$file){
+  registerDoMC(cores=4)
+  foreach(file=files$file) %dopar% {
     
     local.nc.file <- file.path(tempdir(), file)
     
