c48371313cb019e3dddc0417f9979a433f02d79c
Jordan S Read <jread@usgs.gov>
2016-06-23 13:31:41 -0500

    example metadata templating

---
+++ data_release/metadata_create_timeseries.R
@@ -0,0 +1,75 @@
+library(magrittr)
+library(xml2)
+
+
+m <- xml_new_document() %>%
+  xml_add_child("metadata") %>%
+  xml_add_child("idinfo")
+
+m %>%  xml_add_child("citation") %>%
+  xml_add_child("citeinfo") %>%
+  xml_add_child("origin", "{{authors}}") %>%
+  xml_add_sibling('pubdate', "{{pubdate}}") %>%
+  xml_add_sibling('title', "{{title}}") %>%
+  xml_add_sibling('geoform', "text files") %>%
+  xml_add_sibling('onlink', "{{doi}}")
+
+m %>%
+  xml_add_child('descript') %>%
+  xml_add_child("abstract",'{{abstract}}') %>%
+  xml_add_sibling("purpose", '{{purpose}}')
+
+m %>%
+  xml_add_child('timeperd') %>%
+  xml_add_child("timeinfo") %>%
+  xml_add_child("rngdates") %>%
+  xml_add_child('begdate','{{start-date}}') %>%
+  xml_add_sibling('enddate','{{end-date}}')
+m %>%
+  xml_add_child('status') %>%
+  xml_add_child("progress", "Complete") %>% 
+  xml_add_sibling('update','{{update}}') 
+
+m %>% 
+  xml_add_child('spdom') %>% 
+  xml_add_child('bounding') %>% 
+  xml_add_child('westbc', "{{wbbox}}") %>% 
+  xml_add_sibling('eastbc', "{{ebbox}}") %>% 
+  xml_add_sibling('northbc', "{{nbbox}}") %>% 
+  xml_add_sibling('southbc', "{{sbbox}}")
+
+k <- xml_add_child(m, 'keywords') 
+
+k %>% 
+  xml_add_child('theme', "\n<themekt>none</themekt>\n{{#themekeywords}}<themekey>{{.}}</themekey>\n{{/themekeywords}}")
+
+k %>% 
+  xml_add_child('theme') %>% 
+  xml_add_child('themekt','ISO 19115 Topic Category') %>% 
+  xml_add_sibling('themekey','environment') %>% 
+  xml_add_sibling('themekey','inlandWaters') %>% 
+  xml_add_sibling('themekey','007') %>% 
+  xml_add_sibling('themekey','012') 
+
+m %>% 
+  xml_add_child('place') %>% 
+  xml_add_child('placekt','Department of Commerce, 1995, Countries, Dependencies, Areas of Special Sovereignty, and 
+                Their Principal Administrative Divisions,  Federal Information Processing Standard (FIPS) 10-4, 
+                Washington, D.C., National Institute of Standards and Technology') %>% 
+  xml_add_sibling('placekey','United States') %>% 
+  xml_add_sibling('placekey','US')
+m %>% 
+  xml_add_child('place', "\n{{#states}}<placekt>U.S. Department of Commerce, 1987, Codes for the identification of the States, 
+                the District of Columbia and the outlying areas of the United States, and associated areas 
+                (Federal Information Processing Standard 5-2): Washington, D. C., NIST</placekt>
+                <placekey>{{.}}</placekey>\n{{/states}}")
+  
+  
+  
+write_xml(m, file = 'test.xml')
+
+suppressWarnings(readLines('test.xml')) %>% 
+  gsub(pattern = '&gt;',replacement = '>',.) %>% 
+  gsub(pattern = '&lt;',replacement = '<',.) %>% 
+  cat(file = 'test.xml', sep = '\n')
+ 
