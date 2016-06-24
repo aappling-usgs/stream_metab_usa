67c5478744cf2d95ef68024f2953801a62be2fad
Alison Appling <aappling@usgs.gov>
2016-06-24 12:55:18 -0500

    possibly functional remake setup

--- 1_site_data/code/init_site_list.R
+++ 1_site_data/code/init_site_list.R
@@ -3,16 +3,18 @@
 #' @param config a list that configures the rules for sites
 #' @return a character vector of site names as "nwis_{sitename}"
 #' 
-init_site_list <- function(config){
+init_site_list <- function(config, outfile='out/site_list.txt'){
   
-  project.sites <- powstreams::list_sites()
+  project.sites <- mda.streams::list_sites()
   
   fresh.sites <- c()
   hucs <- sprintf('%02.0f',1:21) # all HUCs (PR is 21 BTW)
   for (huc in hucs){
     fresh.sites = c(fresh.sites, get_nwis_sites(huc, config$min.count, config$skip.types, config$has.param))
   }
-  return(unique(c(project.sites, fresh.sites)))
+  sitelist <- unique(c(project.sites, fresh.sites))
+  writeLines(sitelist, outfile)
+  return(sitelist)
 }
 
 
