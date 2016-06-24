92262f4a85e3ac874ed3f85a51f5da5c2eecebec
Jordan S Read <jread@usgs.gov>
2016-06-23 21:22:28 -0500

    more yet...

--- data_release/metadata_create_timeseries.R
+++ data_release/metadata_create_timeseries.R
@@ -66,16 +66,70 @@ k %>%
   
 p <- xml_add_child(m, 'accconst','none')
 
-p %>% 
-  xml_add_sibling('useconst','{{usage-rules}}') %>% 
-  xml_add_sibling('ptcontac') %>% 
+pt <-  xml_add_sibling(p,'ptcontac') 
+
+pt %>% 
   xml_add_child('cntinfo') %>% 
   xml_add_child('cntperp') %>% 
   xml_add_child('cntper','{{contact-person}}') %>% 
   xml_add_sibling('cntorg','U.S. Geological Survey')
 
+p %>% 
+  xml_add_sibling('useconst','{{usage-rules}}')
+
+pt %>% xml_add_child('cntaddr') %>% 
+  xml_add_child('addrtype','Mailing and Physical') %>% 
+  xml_add_sibling('address', '8551 Research Way') %>% 
+  xml_add_sibling('city','Middleton') %>% 
+  xml_add_sibling('state','WI') %>% 
+  xml_add_sibling('postal','53562') %>% 
+  xml_add_sibling('country','U.S.A.')
+
+pt %>%  xml_add_child('cntvoice','{{contact-phone}}') %>% 
+  xml_add_sibling('cntemail','{{contact-email}}')
+
+m %>% 
+  xml_add_child('datacred','{{funding-credits}}') %>% 
+  xml_add_sibling('native','{{build-environment}}') %>% 
+  xml_add_sibling('crossref') %>% 
+  xml_add_child('citeinfo') %>% 
+  xml_add_child('origin','{{cite-authors}}') %>% 
+  xml_add_sibling('pubdate','{{cite-date}}') %>% 
+  xml_add_sibling('title','{{cite-title}}') %>% 
+  xml_add_sibling('geoform','{{paper}}') %>% 
+  xml_add_sibling('pubinfo') %>% 
+  xml_add_child('pubplace',"{{publisher}}") %>% 
+  xml_add_sibling('publish','{{journal}}')
+
+q <- xml_add_sibling(m, 'dataqual')
+
+q %>% xml_add_child('attracc') %>% 
+  xml_add_child('attraccr','No formal attribute accuracy tests were conducted.')
+
+q %>% xml_add_child('logic','not applicable') %>% 
+  xml_add_sibling('complete','not applicable')
+
+p <- xml_add_sibling(q, 'posacc') 
+p %>% xml_add_child('horizpa') %>% 
+  xml_add_child('horizpar','A formal accuracy assessment of the horizontal positional information in the data set has not been conducted.')
+
+p %>% 
+  xml_add_child('vertacc') %>% 
+  xml_add_child('vertaccr','A formal accuracy assessment of the vertical positional information in the data set has either not been conducted, or is not applicable.')
+
+xml_add_sibling(p, 'spdoinfo') %>% 
+  xml_add_child('indspref','{{indirect-spatial}}') %>% 
+  xml_add_sibling('direct','Point') %>% 
+  xml_add_sibling('ptvctinf') %>% 
+  xml_add_child('sdtsterm') %>% 
+  xml_add_child('sdtstype','Point') %>% 
+  xml_add_sibling('ptvctcnt','{{point-count}}')
   
-  
+p %>% xml_add_sibling('lineage') %>% 
+  xml_add_child('procstep') %>% 
+  xml_add_child('procdesc','{{process-description}}') %>% 
+  xml_add_sibling('procdate','{{process-date}}')
+
 write_xml(d, file = 'test.xml')
 
 place.template = "{{#states}}<place><placekt>U.S. Department of Commerce, 1987, Codes for the identification of the States, 
