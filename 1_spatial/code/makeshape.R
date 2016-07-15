58d10bc2afba2d8f2d7be8ab77e3aee855fa153a
Jordan S Read <jread@usgs.gov>
2016-07-15 13:47:49 -0500

    adding multiple CRS

--- 1_spatial/code/makeshape.R
+++ 1_spatial/code/makeshape.R
@@ -12,7 +12,7 @@ spatial_filter_sites <- function(metadata.file){
   # also, filter for sites w/ GPP!
   has.gpp <- metadata$site_name %in% mda.streams::list_sites('gpp_estBest')
   
-  sites <- unitted::v(metadata[has.coord & is.nwis & has.gpp, c("site_name", "lat", "lon" )])
+  sites <- unitted::v(metadata[has.coord & is.nwis & has.gpp, c("site_name", "lat", "lon", "coord_datum")])
   message(nrow(sites),' sites included')
   return(sites)
 }
@@ -21,17 +21,36 @@ spatial_filter_sites <- function(metadata.file){
 #' 
 #' @param sites a data.frame with site ids and lat/lon values (from \code{spatial_filter_sites})
 create_site_points <- function(sites, crs.string = "+init=epsg:4326"){
-  coords <- cbind(sites$lon, sites$lat)
-  points.sp <- sp::SpatialPointsDataFrame(coords, 
-                                         data = sites['site_name'], 
-                                         proj4string = sp::CRS(crs.string))
+
+  crs.strings <- c('NAD83'='+init=epsg:4269', 'WGS84'='+init=epsg:4326')
+  datums <- unique(sites$coord_datum)
   
+  if (!all(datums %in% names(crs.strings))){
+    stop('not all of ', paste(datums,collapse = ', '), ' found in ', paste(names(crs.strings),collapse = ', '))
+  }
+  for (datum in datums){
+    use.i <- sites$coord_datum == datum
+    coords <- cbind(sites$lon[use.i], sites$lat[use.i])
+    raw.points.sp <- sp::SpatialPointsDataFrame(coords, 
+                                                data = data.frame("site_name" = sites$site_name[use.i]), 
+                                                proj4string = sp::CRS(crs.strings[[datum]]))
+    browser()
+    transformed.points.sp <- spTransform(x = raw.points.sp, sp::CRS(crs.string))
+    if (exists('points.sp')){
+      browser()
+      points.sp <- maptools::spRbind(points.sp, transformed.points.sp)
+    } else {
+      points.sp <- transformed.points.sp
+    }
+    
+  }
   return(points.sp)
 }
 
 create_site_catchments <- function(sites, crs.string = "+init=epsg:4326"){
   basin.ids <- mda.streams::parse_site_name(sites$site_name, out='sitenum')
   
+  browser()
   raw.catchments <- hydroMap::getBasin(basin.ids)
   catchment.data <- raw.catchments@data
   updated.data <- catchment.data %>% 
@@ -49,4 +68,8 @@ inventory_map <- function(points, catchments, outfile){
   plot(points[!points@data$site_name %in% catchment.ids, ], col='red', pch=20, cex=0.4, add=TRUE)
   plot(catchments, add=TRUE)
   dev.off()
+}
+
+write_shapefile <- function(obj, fileout){
+  stop('asdf')#writeOGR
 }
