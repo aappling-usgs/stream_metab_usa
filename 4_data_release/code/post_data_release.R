96fd3f0e069b6f8e6023042c9f8a585798290b11
Jordan S Read <jread@usgs.gov>
2016-08-12 13:23:06 -0500

    adding post info and code

--- 4_data_release/code/post_data_release.R
+++ 4_data_release/code/post_data_release.R
@@ -1,8 +1,38 @@
+safe_login <- function(){
+  if (!is_logged_in()){
+    profile <- load_profile()
+    authenticate_sb(profile$sb_user, profile$sb_password)
+  }
+}
+
+create_release_parent <- function(target_name){
+  parent.id <- '57adfa86e4b0fc09faad6d87'
+  create_release_child(parent.id, target_name)
+}
+
+create_release_child <- function(parent.id, target_name){
+  create_release_item(parent.id, key = target_name)
+}
+
+create_release_item <- function(parent.id, key, title = key){
+  if (item_exists(scheme = 'powell_center', type = 'data_release', key = key)){
+    sb.id <- query_item_identifier(scheme = 'powell_center', type = 'data_release', key = key)
+  } else {
+    sb.id <- item_create(parent_id = parent.id, title = title)$id
+    sb.id <- item_update_identifier(item.id, scheme = 'powell_center', type = 'data_release', key = key)$id
+  }
+  return(sb.id)
+}
+
 
 post_spatial <- function(files, target_name){
   spatial.id <- '57adfe26e4b0fc09faad6d97'
   profile <- load_profile()
   authenticate_sb(profile$sb_user, profile$sb_password)
-  item.id <- item_create(parent_id = spatial.id, title = target_name)
-  item_append_files(sb_id = item.id$id, files = files)
+  
+  # item_exists
+  item.id <- item_create(parent_id = spatial.id, title = target_name)$id
+  item_append_files(sb_id = item.id, files = files)
+  site_id <- item_update_identifier(item.id, scheme='mda_streams', type='data_release', key=target_name)
+  return(site_id)
 }
