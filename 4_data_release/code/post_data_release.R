e2e6470f2b228e24b03653ce2608d4dcefe50081
Jordan S Read <jread@usgs.gov>
2016-08-12 15:38:58 -0500

    updates for posting to sb

--- 4_data_release/code/post_data_release.R
+++ 4_data_release/code/post_data_release.R
@@ -1,3 +1,4 @@
+#' login that reads from profile and only does auth if needed
 safe_login <- function(){
   if (!is_logged_in()){
     profile <- load_profile()
@@ -5,34 +6,48 @@ safe_login <- function(){
   }
 }
 
+#' This is where it starts. Use the parent id of where the data release lives
+#' 
+#' @param target_name the title of the parent item to be created
+#' @return a sciencebase ID
 create_release_parent <- function(target_name){
   parent.id <- '57adfa86e4b0fc09faad6d87'
   create_release_child(parent.id, target_name)
 }
 
+#' create a child to any other item in a release
+#' 
+#' @param parent.id sciencebase id of the parent to build off of
+#' @param target_name title of the item to be created
+#' @return a sciencebase ID
 create_release_child <- function(parent.id, target_name){
   create_release_item(parent.id, key = target_name)
 }
 
-create_release_item <- function(parent.id, key, title = key){
+#' worker function for creating release items that are tagged and may have files
+#' 
+#' @param parent.id a sciencebase id of the parent item
+#' @param key the value to use when tagging items
+#' @param \dots optional files to append to the created item
+create_release_item <- function(parent.id, key, ...){
+  safe_login()
   if (item_exists(scheme = 'powell_center', type = 'data_release', key = key)){
-    sb.id <- query_item_identifier(scheme = 'powell_center', type = 'data_release', key = key)
+    sb.id <- query_item_identifier(scheme = 'powell_center', type = 'data_release', key = key)[[1]]$id
   } else {
-    sb.id <- item_create(parent_id = parent.id, title = title)$id
-    sb.id <- item_update_identifier(item.id, scheme = 'powell_center', type = 'data_release', key = key)$id
+    sb.id <- item_create(parent_id = parent.id, title = key)$id
+    item_update_identifier(sb.id, scheme = 'powell_center', type = 'data_release', key = key)
+  }
+  if (length(c(...)) > 0){
+    append_release_files(sb.id, c(...))
   }
   return(sb.id)
 }
 
-
-post_spatial <- function(files, target_name){
-  spatial.id <- '57adfe26e4b0fc09faad6d97'
-  profile <- load_profile()
-  authenticate_sb(profile$sb_user, profile$sb_password)
-  
-  # item_exists
-  item.id <- item_create(parent_id = spatial.id, title = target_name)$id
-  item_append_files(sb_id = item.id, files = files)
-  site_id <- item_update_identifier(item.id, scheme='mda_streams', type='data_release', key=target_name)
-  return(site_id)
+#' append files to a release item
+#' 
+#' @param sb.id a sciencebase id of the item
+#' @param files a list of file paths to be uploaded
+append_release_files <- function(sb.id, files){
+  safe_login()
+  item_replace_files(sb_id = parent.id, files = files, all = FALSE)
 }
