a91484feeeef67aa9170da6e8d199af83ff80f7f
Alison Appling <aappling@usgs.gov>
2017-02-05 18:30:18 -0700

    move skeleton for create_metab_config.R

--- 2_metab_config/code/create_metab_config.R
+++ 2_metab_config/code/create_metab_config.R
@@ -3,6 +3,6 @@
 #' @import mda.streams
 #' 
 #' @param config a config for the config
-create_metab_config <- function(config=yaml::yaml.load_file('2_metab_configs/in/metab_configs_config.yml')) {
+create_metab_config <- function(config=yaml::yaml.load_file('2_metab_config/in/metab_configs_config.yml')) {
   
 }
