f32152c50b0f5409806ee4dd2e0425ad69ccd386
Alison Appling <aappling@usgs.gov>
2017-02-06 17:25:33 -0600

    remember dos2unix for run_job.sh

--- 2_metab_config/cluster/condor/run_job.sh
+++ 2_metab_config/cluster/condor/run_job.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 export PATH=/usr/local/bin:/bin:/usr/bin:/usr/local/sbin:/usr/sbin:/sbin
 
+ls
+
 # unzip the package bundle
 chmod 777 unzip
 ./unzip bundle.zip
@@ -11,6 +13,11 @@ mkdir rLibs
 # put stream_metab.yaml in ~/.R/stream_metab.yaml
 mkdir ~/.R
 mv stream_metab.yaml ~/.R
+
+# make a directory to store results in
+mkdir job
+
+ls
 
 # run the Rscript run_job.R
 Rscript $1 $2
