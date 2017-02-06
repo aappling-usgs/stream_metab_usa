af7edbf080f642eb3356e83117947242964131bf
Alison Appling <aappling@usgs.gov>
2017-02-05 22:37:31 -0700

    work toward functions to manage cluster files & status

---
+++ 2_metab_config/code/cluster_prep_condor.R
@@ -0,0 +1,15 @@
+#' Copy files into a single folder for transfer to HTCondor
+#' 
+#' set up your ~/.R/stream_metab.yaml with fields for the group's sb_user and
+#' sb_password first
+#' 
+#' @param cluster_dir the local folder whose contents you will manually transfer
+cluster_prep_condor <- function(cluster_dir='../2_metab_config/prep/cluster/condor', ...) {
+  
+  files <- c(as.list(...))
+  
+  for(file in files) {
+    success <- file.copy(file, file.path(cluster_dir, basename(file)), overwrite=TRUE)
+    if(!success) stop("file could not be copied: ", file)
+  }
+}
---
+++ 2_metab_config/code/create_metab_table.R
@@ -0,0 +1,19 @@
+#' Create a file to record metab model run/stage/post status
+create_metab_table <- function(config, outfile) {
+  # Only create and update a fresh table if no current one exists. This fixes 
+  # the problem where the table was getting created here, updated in stage_ts, 
+  # and then overwritten by a re-creation because remake notices that the file 
+  # is now different from its hashed file. And stage_ts and sb_post_ts do plenty
+  # of status checking themselves, so no need even to check the status here if
+  # the file already exists.
+  if(!file.exists(outfile)) {
+    model_title <- make_metab_run_title(
+      format(as.Date(config[,'date']), '%y%m%d'), config[,'tag'], config[,'strategy'])
+    model_name <- make_metab_model_name(model_title, 1:nrow(config), config[,'site'])
+    make_metab_model_path(model_name, '.')
+    
+    # create and write the model status table
+    metab.table <- data.frame(name=model_title, local=FALSE, error=FALSE, posted=FALSE, tagged=FALSE)
+    write_status_table(site.table, outfile)
+  }
+}
