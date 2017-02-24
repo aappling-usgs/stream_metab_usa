32d408af70f3e0080bb47d205293e070e43cd798
Alison Appling <aappling@usgs.gov>
2017-02-24 10:52:38 -0600

    whoops had to restart 2/21 run b/c of this

--- 2_metab_config/code/run_model.R
+++ 2_metab_config/code/run_model.R
@@ -12,7 +12,7 @@ run_model <- function(config_row, verbose, outdir, model_name) {
   print(config_row)
   if(verbose) message('running model')
   model_out <- config_to_metab(config=config_row, rows=1, verbose=verbose)[[1]]
-  saveRDS(metab_out, file.path(outdir, sprintf("partial %s.Rds", model_name)))
+  saveRDS(model_out, file.path(outdir, sprintf("partial %s.Rds", model_name)))
   if(verbose) message('printing model')
   tryCatch({
     print(class(model_out))
