17e1ef8874e4dcad9515a66a524d162bd16bd512
Alison Appling <aappling@usgs.gov>
2017-04-27 15:42:57 -0700

    make model tses

--- 2_metab_models/run3/code/make_model_tses.R
+++ 2_metab_models/run3/code/make_model_tses.R
@@ -1,6 +1,11 @@
 #' write metab ts files for the current model=resolution (could be several
 #' resolutions per site)
 make_model_tses <- function(model_out, outdir) {
+  
+  library(methods) # necessary when running via 'Rscript' call at command line
+  library(streamMetabolizer)
+  library(mda.streams)
+  
   tsfiles <- stage_metab_ts(model_out, folder=outdir)
   res <- substring(get_info(model_out)$config$strategy, 7)
   sapply(tsfiles, function(tsfile) {
