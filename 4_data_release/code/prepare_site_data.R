744f1a129e0dfef7e691245f51a9a4979da7d1f9
Alison Appling <aappling@usgs.gov>
2017-09-18 13:15:41 -0700

    remake for generating one big site_data metadata file

--- 4_data_release/code/prepare_site_data.R
+++ 4_data_release/code/prepare_site_data.R
@@ -1,6 +1,19 @@
-
+#' for all site metadata files listed in ..., determine the types, download from
+#' sciencebase, and combine and rename and filter into a nice single table
+#' relevant to exactly the sites we want
 combine_site_data <- function(..., outfile) {
+  library(mda.streams)
+  library(dplyr)
+  library(unitted)
+
   infiles <- list(...)
+  meta_types <- parse_meta_path(unlist(infiles))$type
+  meta <- get_meta(meta_types) %>% # pull from sciencebase because get_meta adds useful prefixes
+    select(-site_database, -sciencebase_id) %>%
+    select(site_name, nwis_id=site_num, nhdplus_id, everything())
   
-  print(infiles)
+  # units <- get_units(meta)
+  # out <- deunitted(meta)
+  
+  return(meta) # or list(units, out)?
 }
