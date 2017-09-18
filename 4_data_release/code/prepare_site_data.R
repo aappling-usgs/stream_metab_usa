ac5a3a113a5999b8f1f69171a4cea986c7b571e6
Alison Appling <aappling@usgs.gov>
2017-09-18 13:53:50 -0700

    complete draft of releasing site_data

--- 4_data_release/code/prepare_site_data.R
+++ 4_data_release/code/prepare_site_data.R
@@ -1,19 +1,22 @@
 #' for all site metadata files listed in ..., determine the types, download from
 #' sciencebase, and combine and rename and filter into a nice single table
 #' relevant to exactly the sites we want
-combine_site_data <- function(..., outfile) {
+combine_site_data <- function(...) {
   library(mda.streams)
   library(dplyr)
   library(unitted)
 
-  infiles <- list(...)
-  meta_types <- parse_meta_path(unlist(infiles))$type
+  meta_types <- c(...)
   meta <- get_meta(meta_types) %>% # pull from sciencebase because get_meta adds useful prefixes
     select(-site_database, -sciencebase_id) %>%
     select(site_name, nwis_id=site_num, nhdplus_id, everything())
   
-  # units <- get_units(meta)
-  # out <- deunitted(meta)
+  return(meta)
+}
+
+write_site_data <- function(meta, outfile) {
   
-  return(meta) # or list(units, out)?
+  out <- deunitted(meta)
+  write.table(out, file=outfile, sep='\t', row.names=FALSE, quote=TRUE)
+  
 }
