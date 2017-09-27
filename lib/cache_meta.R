cf2140f909c36a89712a135d34a4a1f66374567e
Alison Appling <aappling@usgs.gov>
2017-09-27 16:43:34 -0700

    remove duplicate sb_meta_struct target & code

--- lib/cache_meta.R
+++ lib/cache_meta.R
@@ -13,6 +13,17 @@ cache_meta <- function(type, config, ...) {
     type,
     basic    = stage_meta_basic(folder=folder, ...),
     dvqcoefs = stage_meta_dvqcoefs(folder=folder, ...),
-    struct   = stage_meta_struct(folder=folder, ...)
+    struct   = {
+      # here's a shortcut if the mfootdaily time series estimates are stored in a
+      # local cache - lets you not re-download all those tses from sciencebase
+      args <- list(...)
+      use_cached <- args[['use_cached']]
+      if(isTRUE(use_cached)) {
+        ts_folder <- "../2_metab_outputs/cache"
+        mf_files <- dir(ts_folder, pattern='mfootdaily', full.names=TRUE)
+        file.copy(mf_files, tempdir())
+      }
+      stage_meta_struct(folder=folder, struct_file=args$struct_file, verbose=args$struct_file)
+    }
   )
 }
