4e68830d8285ca4576ad05349c992bcde40b9cc6
Jordan S Read <jread@usgs.gov>
2017-09-28 15:40:38 -0500

    templated make for timeseries postings

--- 4_timeseries_release/code/download_release.R
+++ 4_timeseries_release/code/download_release.R
@@ -1,20 +1,44 @@
 
-#' if file exists locally, skip. 
-#' if file fails to get remote location, fail
-download_release_ts <- function(file.path){
+#' figure out what files are ready to be downloaded from SB
+#' @param target_name the nwis_{site.id}_... target name. Format matters, since we parse it
+#' @return a vector of file names for this site
+files_for_download <- function(target_name, ts.dir){
   
-  parsed.path <- mda.streams::parse_ts_path(file.path, out = c("dir_name", "file_name", "var_src", "site_name"))
-  mda.streams::download_ts(var_src = parsed.path$var_src, site_name = parsed.path$site_name, 
-                           folder = parsed.path$dir_name, on_local_exists = 'skip', on_remote_missing = 'stop')
+  library(dplyr)
+  match.id <- paste0(paste(strsplit(target_name, '[_]')[[1]][1:2], collapse = '_'), '-')
+  ts.files <- dir(ts.dir, full.names=TRUE) %>% {.[!grepl('all_ts_files|Thumbs.db', .)]}
+  file.list <- c()
+  for (file in ts.files){
+    file.meta <- readr::read_tsv(file) %>%  filter(grepl(filepath, pattern = match.id))
+    if (nrow(file.meta) == 1 && all(file.meta$posted, file.meta$tagged)){
+      file.list <- c(file.list, file.meta$filepath)
+    } else if (nrow(file.meta) > 1){
+      stop('something is wrong, multiple file matches for ', target_name)
+    }
+  }
+  return(file.list)
 }
 
+#' find all files that need to be local for a site, DL them
+#' 
+#' @param site.id the nwis_{site.no} to be used
+#' @return a vector of file paths for the downloaded files
 
-merge_and_post_sb <- function(target.name, parent.id, ...){
+download_release_tses <- function(file.paths){
+  for (file.path in file.paths){
+    parsed.path <- mda.streams::parse_ts_path(file.path, out = c("dir_name", "file_name", "var_src", "site_name"))
+    mda.streams::download_ts(var_src = parsed.path$var_src, site_name = parsed.path$site_name, 
+                             folder = parsed.path$dir_name, on_local_exists = 'skip', on_remote_missing = 'stop')
+  }
+  return(file.paths)
+}
+
+post_release_tses <- function(target.name, parent.id, file.paths){
   
   tmpdir <- tempdir()
   zipfile <- file.path(tmpdir, paste0(target.name, '.zip'))
   
-  rds.files <- c(...)
+  rds.files <- file.paths
   tsv.files <- c()
   for (rds.file in rds.files){
     parsed.path <- mda.streams::parse_ts_path(rds.file, out = c("dir_name", "file_name", "var", "src", "site_name", "ts_name"))
