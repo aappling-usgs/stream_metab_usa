51ea4f74bffa71aeeb2d0ff3079c9f582db9fef3
Alison Appling <aappling@usgs.gov>
2017-09-29 12:25:53 -0700

    modify release_models for new create_task_makefile features

--- 4_data_release/code/model_release_tasks.R
+++ 4_data_release/code/model_release_tasks.R
@@ -1,7 +1,13 @@
-# model_folder gets used in a couple of functions in this script
-model_folder <- '../4_data_release/cache/models'
-
 create_model_release_task_plan <- function(metab.config) {
+
+  # define & create model_folder & indicator_folder as needed. wait to create
+  # them until we're within a function because when remake sources this file,
+  # our working directory will be set to the directory of this file, and that's
+  # confusing
+  model_folder <- '../4_data_release/cache/models'
+  indicator_folder <- '../4_data_release/log/models'
+  if(!dir.exists(model_folder)) dir.create(model_folder)
+  if(!dir.exists(indicator_folder)) dir.create(indicator_folder)
   
   # define the tasks as unique IDs for each model
   model_titles <- make_metab_run_title(
@@ -17,7 +23,6 @@ create_model_release_task_plan <- function(metab.config) {
     model_name=model_names, folder=model_folder) %>%
     setNames(model_names)
   
-  if(!dir.exists(model_folder)) dir.create(model_folder)
   download <- create_task_step(
     step_name = 'download',
     target = function(task_name, step_name, ...) {
@@ -27,7 +32,7 @@ create_model_release_task_plan <- function(metab.config) {
       model_path <- download_paths[[task_name]]
       model_name <- parse_metab_model_path(model_path, out='model_name')
       sprintf(
-        "download_model(model_name=I('%s'))",
+        "download_model(model_name=I('%s'), model_folder=I('%s'))",
         model_name, model_folder)
     }
   )
@@ -57,23 +62,24 @@ create_model_release_task_plan <- function(metab.config) {
   #   target = target_fun
   # )
   
-  task_plan <- create_task_plan(model_names, list(download, inputs))
+  task_plan <- create_task_plan(
+    model_names, list(download, inputs),
+    final_steps='inputs', indicator_dir=indicator_folder)
 }
 
 create_model_release_makefile <- function(makefile, task_plan, template_file='../lib/task_makefile.mustache') {
-  st_dir <- '../4_data_release/log'
-  if(!dir.exists(st_dir)) dir.create(st_dir)
+  ind_dir <- '../4_data_release/log'
+  if(!dir.exists(ind_dir)) dir.create(ind_dir)
   create_task_makefile(
     makefile=makefile, task_plan=task_plan,
-    indicator_file = file.path(st_dir, 'model_release.st'),
-    job_steps = 'inputs',
-    include = '4_release_models.yml',
+    indicator_dir=dirname(attr(task_plan, 'indicator_dir')),
+    include='4_release_models.yml',
     packages=c('mda.streams', 'streamMetabolizer', 'dplyr'),
-    file_extensions=c('st','RData'),
+    file_extensions=c('ind','RData'),
     template_file=template_file)
 }
 
-download_model <- function(model_name) {
+download_model <- function(model_name, model_folder) {
   mda.streams::login_sb()
   download_metab_model(
     model_name=model_name, folder=model_folder, version='original',
