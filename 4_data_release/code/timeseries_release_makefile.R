8f42314bace23219366835a73b0873bfaccd7085
Alison Appling <aappling@usgs.gov>
2017-10-02 09:56:19 -0700

    use new indicator file system

--- 4_data_release/code/timeseries_release_makefile.R
+++ 4_data_release/code/timeseries_release_makefile.R
@@ -1,4 +1,4 @@
-create_timeseries_release_makefile <- function(makefile){
+create_timeseries_release_makefile <- function(makefile, template_file){
   
   ts.dir <- "../1_timeseries/out"
   
@@ -16,7 +16,7 @@ create_timeseries_release_makefile <- function(makefile){
   )
   
   timeseries <- create_task_step(
-    step_name = '.dummy',
+    step_name = 'ts_posted',
     command = function(target_name, task_name, ...) {
       sprintf('post_release_tses(target_name, pc1_timeseries, %s_ts_downloaded)', task_name)
     },
@@ -36,17 +36,19 @@ create_timeseries_release_makefile <- function(makefile){
     ts.sites <- unique(c(ts.sites, ts.file.sites))
   }
   
-  task_plan <- create_task_plan(ts.sites, list(list_files, download_ts, timeseries))
+  ind_dir <- '../4_data_release/log/timeseries'
+  if(!dir.exists(ind_dir)) dir.create(ind_dir, recursive=TRUE)
   
+  task_plan <- create_task_plan(
+    ts.sites, list(list_files, download_ts, timeseries),
+    final_steps=c('ts_posted'), indicator_dir=ind_dir)
   
-  
-  create_task_makefile(task_plan = task_plan, 
-                       job_target = '4_release_timeseries', 
-                       include = '4_release_parent.yml', 
-                       packages = c('mda.streams', 'readr', 'dplyr'),
-                       sources = c('../lib/load_profile.R','../4_timeseries_release/code/download_release.R', '../4_data_release/code/post_data_release.R'), 
-                       makefile = makefile) 
-  
+  create_task_makefile(
+    makefile=makefile,
+    task_plan=task_plan,
+    indicator_dir=dirname(ind_dir),
+    include='4_release_timeseries.yml',
+    packages=c('mda.streams', 'readr', 'dplyr'),
+    sources = c('../lib/load_profile.R','../4_timeseries_release/code/download_release.R', '../4_data_release/code/post_data_release.R'), 
+    template_file=template_file)
 }
-
-
