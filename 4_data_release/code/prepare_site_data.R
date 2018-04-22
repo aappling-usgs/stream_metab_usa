f7b84578213de0649759c87c8947e57fc6e5bdf6
Alison Appling <aappling@usgs.gov>
2018-04-22 12:39:04 -0700

    resolve #269 site details for all spatial+modeled sites

--- 4_data_release/code/prepare_site_data.R
+++ 4_data_release/code/prepare_site_data.R
@@ -2,15 +2,12 @@
 #' sciencebase, and combine and rename and filter into a nice single table
 #' relevant to exactly the sites we want
 combine_site_data <- function(..., sites) {
-  library(mda.streams)
-  library(dplyr)
-  library(unitted)
 
   meta_types <- c(...)
   meta <- get_meta(meta_types) %>% # pull from sciencebase because get_meta adds useful prefixes
     select(-site_database, -sciencebase_id) %>%
     select(site_name, nwis_id=site_num, nhdplus_id, everything()) %>%
-    {.[.$site_name %in% sites,]} # dplyr::filter makes a warning with unitted
+    {.[.$site_name %in% sites$site_name,]} # dplyr::filter makes a warning with unitted
   
   return(meta)
 }
