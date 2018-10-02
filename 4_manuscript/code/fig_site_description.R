8c94b02c7e97966f0984fd564f3be6ba6ab2aaac
Alison Appling <aappling@usgs.gov>
2018-10-02 16:29:14 -0400

    add fig 3, revise site map, get more stats

--- 4_manuscript/code/fig_site_description.R
+++ 4_manuscript/code/fig_site_description.R
@@ -46,23 +46,28 @@ prep_spatial_points <- function(points, dailies, diagnostics) {
 panel_spatial_points <- function(points_prepped) {
   # prepare aesthetic info (color, legend)
   points_w_aes <- points_prepped
-  year_breaks <- c(-0.00000000001, min(points_w_aes@data$years, na.rm=TRUE), 1,3,5,7, max(points_w_aes@data$years, na.rm=TRUE))
-  year_colors <- c('grey60', viridisLite::viridis(length(year_breaks)-2, alpha=1, begin=0, end=1, direction=-1))
+  #year_breaks <- c(-0.00000000001, min(points_w_aes@data$years, na.rm=TRUE), 1,3,5,7, max(points_w_aes@data$years, na.rm=TRUE))
+  day_breaks <- c(-0.00000000001, min(points_w_aes@data$days, na.rm=TRUE), 365, 1096, 1926, 2557, max(points_w_aes@data$days, na.rm=TRUE))
+  day_colors <- c('grey60', viridisLite::viridis(length(day_breaks)-2, alpha=1, begin=0, end=1, direction=-1))
   points_w_aes@data <- points_w_aes@data %>%
     mutate(
-      years = ifelse(is.na(years), 0, years),
-      year_bin = cut(years, breaks=year_breaks),
-      year_bin_num = as.numeric(year_bin),
-      color = year_colors[year_bin_num],
-      shape = ifelse(year_bin_num == 1, 24, 19),
-      size = ifelse(year_bin_num == 1, 0.4, 0.6)
+      # years = ifelse(is.na(years), 0, years),
+      # year_bin = cut(years, breaks=year_breaks),
+      # year_bin_num = as.numeric(year_bin),
+      days = ifelse(is.na(days), 0, days),
+      day_bin = cut(days, breaks=year_breaks),
+      day_bin_num = as.numeric(day_bin),
+      color = day_colors[day_bin_num],
+      shape = ifelse(day_bin_num == 1, 24, 19),
+      size = ifelse(day_bin_num == 1, 0.4, 0.6)
     )
   points_legend <- list(
-    color = year_colors,
-    shape = c(24, rep(19, length(year_colors)-1)),
-    size = c(0.4, rep(0.6, length(year_colors)-1)),
-    label = data_frame(lo=year_breaks[-length(year_breaks)], hi=year_breaks[-1]) %>%
-      mutate(rng=sprintf('%s to %s', signif(lo, digits=2), signif(hi, digits=3)),
+    color = day_colors,
+    shape = c(24, rep(19, length(day_colors)-1)),
+    size = c(0.4, rep(0.6, length(day_colors)-1)),
+    label = data_frame(lo=day_breaks[-length(day_breaks)], hi=day_breaks[-1]) %>%
+      #mutate(rng=sprintf('%s to %s', signif(lo, digits=2), signif(hi, digits=3)),
+      mutate(rng=sprintf('%s to %s', round(lo), round(hi)),
              rng=c('0', rng[-1])) %>%
       pull(rng))
   # project and plot - uses functions & globals from spatial_plotting.R
@@ -77,7 +82,7 @@ panel_spatial_points <- function(points_prepped) {
   )
   par(new=TRUE, fig=c(0.7, 1, 0.24, 0.88), oma=c(0,0,0,0))
   plot.new()
-  legend(title='Number of metabolism\nestimates in years', x=0.05, y=0.95,
+  legend(title='Number of daily\nmetabolism estimates', x=0.05, y=0.95,
          legend=points_legend$label, 
          pch=points_legend$shape, pt.cex=points_legend$size*4/3,
          col=points_legend$color, bg=points_legend$color,
@@ -100,7 +105,7 @@ fig_site_description <- function(
   points_prepped <- prep_spatial_points(points, dailies, diagnostics)
   
   # start the plot file
-  pdf(file = outfile, width = 7, height = 3)
+  pdf(file = outfile, width = 6, height = 3)
   par(mar=c(0,0,0,0), omi=c(0.2,0,0,0))
   plot.new()
   
@@ -111,8 +116,6 @@ fig_site_description <- function(
   dev.off()
 }
 
-
-#### not used ####
 
 prep_temporal_coverage <- function(dailies) {
   
@@ -130,11 +133,12 @@ prep_temporal_coverage <- function(dailies) {
           end_pos = cumsum(lengths) + 1,
           start_pos = c(1, end_pos[-n()]),
           start_date = thesedates[start_pos],
-          end_date = thesedates[end_pos]
+          end_date = thesedates[end_pos],
+          run='multiday'
         )
     }) %>%
     filter(values==1) %>%
-    select(site_name, start_date, end_date)
+    select(site_name, start_date, end_date, run)
   # find the single days with multi-day gaps on either side
   solodays <- dailies %>%
     group_by(site_name) %>%
@@ -142,11 +146,14 @@ prep_temporal_coverage <- function(dailies) {
     do(., {
       thesedates <- .$date
       diffs <- as.numeric(diff(thesedates), units="days")
-      solodates <- thesedates[which(diffs > 1 & lag(diffs) > 1)]
-      data_frame(start_date=solodates, end_date=solodates+0.5)
+      solodates <- thesedates[which(diffs > 1 & (is.na(lag(diffs)) | lag(diffs) > 1))]
+      data_frame(start_date=solodates, end_date=solodates+0.5, run='single')
     })
   # combine
   runlines <- bind_rows(longruns, solodays) %>%
+    mutate(
+      start_date = as.POSIXct(start_date) %>% lubridate::with_tz('UTC'),
+      end_date = as.POSIXct(end_date) %>% lubridate::with_tz('UTC')) %>%
     arrange(site_name, start_date) %>%
     ungroup()
   
@@ -162,16 +169,17 @@ prep_temporal_coverage <- function(dailies) {
       span_years = span_days / 365.25,
       mid = start + as.difftime(span_days/2, units='days'),
       n_jan = length(date[format(date, '%m') == '01']),
-      seasonal = if(n_jan/days < (1/24)) "Seasonal" else  "Year-Round",
+      seasonal = if(n_jan/days < (1/24)) "Seasonal" else  "Non-seasonal",
       peryear = days / span_years,
       peryear_bin = cut(peryear, breaks=c(17,100,200,300,366))
     ) %>%
     ungroup() %>%
+    mutate(seasonal = ordered(seasonal, c('Seasonal','Non-seasonal'))) %>%
     group_by(seasonal) %>%
-    arrange(start) %>%
+    arrange(start, desc(peryear)) %>%
     mutate(site_rank_seasonal = 1:n()) %>%
     ungroup() %>%
-    arrange(start) %>%
+    arrange(start, desc(peryear)) %>%
     mutate(site_rank_all = 1:n())
   
   # combine runlines and site rankings
@@ -181,43 +189,62 @@ prep_temporal_coverage <- function(dailies) {
 }
 
 panel_temporal_coverage <- function(dailies_prepped) { 
-  # prepare data and dummy data for generating plot in base R
-  lims_df <- data.frame(
-    date = c(min(runlines$start_date), max(runlines$end_date)),
-    y = c(0,nrow(counts)),
-    dummy = NA)
-  bounds_df <- data.frame(
-    date = as.Date(sprintf('%d-10-01', seq(2007, 2016, by=3))))
-  runs_legend <- list(
-    label = gsub(',', '-', gsub('\\(|\\]', '', levels(ranked_runs$peryear_bin))),
-    color = c('#e66101','#fdb863','#b2abd2','#5e3c99'),
-    x = min(lims_df$date) + 1.2*diff(range(lims_df$date)),
-    y = min(lims_df$y) + 1*diff(range(lims_df$y)))
-  colored_runs <- ranked_runs %>%
-    mutate(peryear_col = runs_legend$color[peryear_bin])
   
   # Plot: Duration and frequency of daily metabolism observations
-  par(new=TRUE, mar=c(1.5, 4, 0.25, 12), fig=c(0, 1, 0.1, 0.5), xpd=TRUE)
-  plot(dummy ~ date, data=lims_df, ylim=range(lims_df$y), yaxt='n', xaxt='n', xlab=NA, ylab='Site Rank by Start Date', bty='l', cex.lab=0.9)
-  axis(1, bounds_df$date, format(bounds_df$date, '%Y-%m'), cex.axis=0.9)
-  axis(2, c(0,120,240,356), cex.axis=0.9)
-  segments(
-    x0=colored_runs$start_date, x1=colored_runs$end_date,
-    y0=colored_runs$site_rank_all, y1=colored_runs$site_rank_all,
-    col=colored_runs$peryear_col)
-  legend(title='Values per Year', x=runs_legend$x, y=runs_legend$y, legend=runs_legend$label, col=runs_legend$color, lty=1, bg=NA, bty='n', y.intersp=1, cex=0.9)
-  
-  # ggplot(ranked_runs) +
-  #   geom_segment(aes(x=start_date, xend=end_date, y=site_rank_all, yend=site_rank_all, color=peryear_bin)) +
-  #   scale_colour_manual('Days per Year', values=c('#e66101','#fdb863','#b2abd2','#5e3c99')) + #http://colorbrewer2.org/#type=diverging&scheme=PuOr&n=4
-  #   scale_y_continuous('Site Rank by Start Date', breaks=c(0,120,240,356)) +
-  #   xlab('Date') +
-  #   theme_classic() +
-  #   theme(legend.position=c(0.2,0.75))
+  ggplot(dailies_prepped) +
+    # one color:
+    # geom_segment(aes(x=start_date, xend=end_date, y=site_rank_seasonal, yend=site_rank_seasonal), color='royalblue') +
+    # four colors:
+    # geom_segment(aes(x=start_date, xend=end_date, y=site_rank_seasonal, yend=site_rank_seasonal, color=peryear_bin), size=0.3) +
+    # scale_colour_manual('Days per Year', values=c('#e66101','#fdb863','#b2abd2','#5e3c99')) + #http://colorbrewer2.org/#type=diverging&scheme=PuOr&n=4
+    # many colors:
+    geom_segment(aes(x=start_date, xend=end_date, y=site_rank_seasonal, yend=site_rank_seasonal, color=peryear), size=0.4) +
+    scale_colour_gradient('Estimates per year', low='#fdb863', high='#5e3c99', limits=range(dailies_prepped$peryear)) +
+    guides(colour = guide_colorbar(draw.ulim = FALSE,draw.llim = TRUE)) +
+    scale_y_continuous('Site rank by start date') +
+    xlab('Date') +
+    theme_classic() +
+    theme(legend.position=c(0.2,0.5), legend.key.height=unit(1, 'line'), strip.background=element_rect(fill=NA, color=NA)) +
+    facet_grid(seasonal ~ ., scales='free_y', space='free_y')
 }
 
-# points_w_aes@data %>% ggplot(aes(y=years*365.25, x=span_years)) +
+fig_temporal_coverage <- function(
+  outfile='../4_manuscript/fig/dates.pdf',
+  daily_zip='../4_data_release/cache/models/post/daily_predictions.zip')
+{
+  # load the file
+  unzipped <- unzip(zipfile=daily_zip, exdir=file.path(tempdir(), 'temporal_coverage'))
+  dailies <- readr::read_tsv(
+    unzipped, col_types = readr::cols(
+      .default = col_double(),
+      site_name = col_character(),
+      resolution = col_integer(),
+      date = col_date(format = "")
+    ))
+  
+  # prepare the plotting data
+  dailies_prepped <- prep_temporal_coverage(dailies)
+  
+  # plot and save
+  g <- panel_temporal_coverage(dailies_prepped)
+  ggsave(plot = g, filename = outfile, width = 5, height = 5.5)
+}
+
+# ggplot(dailies_prepped$ranked_runs) +
+#   geom_segment(aes(x=start_date, xend=end_date, y=site_rank_all, yend=site_rank_all, color=peryear_bin)) +
+#   scale_colour_manual('Days per Year', values=c('#e66101','#fdb863','#b2abd2','#5e3c99')) + #http://colorbrewer2.org/#type=diverging&scheme=PuOr&n=4
+#   scale_y_continuous('Site Rank by Start Date', breaks=c(0,120,240,356)) +
+#   xlab('Date') +
+#   theme_classic() +
+#   theme(legend.position=c(0.2,0.75))
+# ggplot(dailies_prepped, aes(x=peryear)) + geom_density(color=NA, fill='gray') + theme_classic()
+# dailies_prepped %>% ggplot(aes(y=years*365.25, x=span_years)) +
 #   geom_abline(slope=365.25) +
 #   geom_point() + theme_classic() +
 #   xlab('Span of metabolism estimates (years)') +
 #   ylab('Count of daily metabolism estimates (days)')
+# dailies_prepped %>% ggplot(aes(y=peryear, x=span_years)) +
+#   geom_hline(yintercept=365.25) +
+#   geom_point() + theme_classic() +
+#   xlab('Span of metabolism estimates (years)') +
+#   ylab('Density of daily metabolism estimates\n(model dates divided by date range)')
